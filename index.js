2d202856216880a74a2fa09a434cbc88bce538a7
IGNACIO GUTMAN <46917360@ort>
2023-05-29 09:04:47 -0300

    si

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get("/API", async (req, res) => {
+app.get("/api/pizzas", async (req, res) => {
   try {
     let GetAll = await svc.getAll();
     res.send(GetAll);
@@ -19,7 +19,7 @@ app.get("/API", async (req, res) => {
   }
 });
 
-app.get("/getById/:id", async (req, res) => {
+app.get("/api/pizzas/:id", async (req, res) => {
   try {
     let GetById = await svc.getById(req.params.id);
     res.send(GetById);
@@ -28,7 +28,7 @@ app.get("/getById/:id", async (req, res) => {
   }
 });
 
-app.delete("/delete/:id", async (req, res) => {
+app.delete("/api/pizzas/:id", async (req, res) => {
   try {
     let deleteById = await svc.deleteById(req.params.id);
     res.send(deleteById);
@@ -37,14 +37,15 @@ app.delete("/delete/:id", async (req, res) => {
   }
 });
 
-app.post("/insert", async (req, res) => {
+app.post("/api/pizzas", async (req, res) => {
   try {
+    console.log(req.body.nombre);
     let svc2 = new Pizza();
     let pizzaNew = new svc2.constructor(
-      req.query.nombre,
-      req.query.libregluten,
-      req.query.importe,
-      req.query.descripcion
+      req.body.nombre,
+      req.body.libregluten,
+      req.body.importe,
+      req.body.descripcion
     );
     let Insert = await svc.insert(pizzaNew);
     res.send(Insert);
@@ -53,7 +54,7 @@ app.post("/insert", async (req, res) => {
   }
 });
 
-app.put("/update", async (req, res) => {
+app.put("/api/pizzas/:id", async (req, res) => {
   try {
     console.log(req.body.Nombre);
     let svc2 = new Pizza();
--- public/site.js
+++ public/site.js
@@ -1,34 +1,34 @@
-function home(){
-    var functions = document.querySelectorAll('.function');
-    functions.forEach((func) => {
-        func.style = "display: inline-block";
-    });
-    document.getElementById('getAll').style = "display: inline";
-    document.querySelector(".lista").innerHTML = '';
-    document.getElementById('back').style = "display: none;"
-    document.getElementById('idSelectorGet').style = "display: inline";
-    document.getElementById('getById').style = "display: inline";
-    document.getElementById('idSelectorDel').style = "display: inline";
+function home() {
+  var functions = document.querySelectorAll('.function');
+  functions.forEach((func) => {
+    func.style = "display: inline-block";
+  });
+  document.getElementById('getAll').style = "display: inline";
+  document.querySelector(".lista").innerHTML = '';
+  document.getElementById('back').style = "display: none;"
+  document.getElementById('idSelectorGet').style = "display: inline";
+  document.getElementById('getById').style = "display: inline";
+  document.getElementById('idSelectorDel').style = "display: inline";
 }
 
-function hide(){
-    document.getElementById('getAll').style = "display: none";
-    document.getElementById('idSelectorGet').style = "display: none";
-    document.getElementById('getById').style = "display: none";
-    var functions = document.querySelectorAll('.function');
-    functions.forEach((func) => {
-        func.style = "display: none";
-    });
-    document.getElementById('idSelectorDel').style = "display: none";
+function hide() {
+  document.getElementById('getAll').style = "display: none";
+  document.getElementById('idSelectorGet').style = "display: none";
+  document.getElementById('getById').style = "display: none";
+  var functions = document.querySelectorAll('.function');
+  functions.forEach((func) => {
+    func.style = "display: none";
+  });
+  document.getElementById('idSelectorDel').style = "display: none";
 }
 
-function getAll(){
-    document.getElementById('back').style = "display: block;"
-    hide();
-    axios
-    .get("http://localhost:912/API")
+function getAll() {
+  document.getElementById('back').style = "display: block;"
+  hide();
+  axios
+    .get("http://localhost:912/api/pizzas")
     .then((result) => {
-        
+
       pizzas = result.data[0];
       pizzas.map((pizza) => {
         const { Id, Nombre, LibreGluten, Importe, Descripcion } = pizza;
@@ -42,7 +42,7 @@ function getAll(){
             </li>
         `;
       });
-      
+
     })
     .catch((error) => {
       console.log('error');
@@ -50,14 +50,14 @@ function getAll(){
 
 }
 
-function getById(){
-    document.getElementById('back').style = "display: block;";
-    hide();
+function getById() {
+  document.getElementById('back').style = "display: block;";
+  hide();
 
-    const id = document.getElementById('idSelectorGet').value;
-    
-    axios
-    .get(`http://localhost:912/getById/${id}`)
+  const id = document.getElementById('idSelectorGet').value;
+
+  axios
+    .get(`http://localhost:912/api/pizzas/${id}`)
     .then((result) => {
       console.log(result.data);
       pizza = result.data;
@@ -72,76 +72,88 @@ function getById(){
                 </li>
             </center>
         `;
-      
+
     })
     .catch((error) => {
       console.log('error');
     });
 }
 
-function deleteId(){
-    const id = document.getElementById('idSelectorDel').value;
-    
-    axios
-    .delete(`http://localhost:912/delete/${id}`)
+function deleteId() {
+  const id = document.getElementById('idSelectorDel').value;
+
+  axios
+    .delete(`http://localhost:912/api/pizzas/${id}`)
     .then((result) => {
-        
+
       alert("Borrado con exito");
-      
+
     })
     .catch((error) => {
       console.log('error');
     });
 }
 
-function insert(){
-    const pizza = {
-        nombre: document.getElementById('nombreInsert').value,
-        libreGluten: document.getElementById('libreGlutenInsert').checked,
-        importe: document.getElementById('importeInsert').value,
-        descripcion: document.getElementById('descripcionInsert').value
-    }
+function insert() {
+  const params = JSON.stringify({
+    nombre: document.getElementById('nombreInsert').value,
+    libreGluten: document.getElementById('libreGlutenInsert').checked,
+    importe: document.getElementById('importeInsert').value,
+    descripcion: document.getElementById('descripcionInsert').value
+  });
 
-    axios
-    .post(`http://localhost:912/insert?nombre=${pizza.nombre}&libregluten=${pizza.libreGluten}&importe=${pizza.importe}&descripcion=${pizza.descripcion}`)
-    .then((result) => {
-        
-      alert("Insertado con exito");
-      
-    })
-    .catch((error) => {
-      console.log('error');
-    });
 
-}
+  const options = {
+    method: 'POST',
+    url: `http://localhost:912/api/pizzas`,
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidApi-Key': 'your-rapidapi-key',
+      'X-RapidApi-Host': 'microsoft-translator-text.p.rapidapi.com',
+    },
+    data: params
+  }
 
-function update(){
-
-    let params = JSON.stringify({
-        "Id": document.getElementById('idUpdate').value,
-        "Nombre": document.getElementById('nombreUpdate').value,
-        "LibreGluten": document.getElementById('libreGlutenUpdate').checked,
-        "Importe": document.getElementById('importeUpdate').value,
-        "Descripcion": document.getElementById('descripcionUpdate').value
-    });
-
-    const options = {
-        method: 'PUT', 
-        url: `http://localhost:912/update`,
-        headers: {
-            'content-type': 'application/json',
-            'X-RapidApi-Key': 'your-rapidapi-key',
-            'X-RapidApi-Host': 'microsoft-translator-text.p.rapidapi.com',
-        },
-        data: params
-    }
-
-    axios
+  axios
     .request(options)
     .then((result) => {
-        
+
+      alert("Insertado con exito");
+
+    })
+    .catch((error) => {
+      console.log('error');
+    });
+
+}
+
+function update() {
+
+  let params = JSON.stringify({
+    "Id": document.getElementById('idUpdate').value,
+    "Nombre": document.getElementById('nombreUpdate').value,
+    "LibreGluten": document.getElementById('libreGlutenUpdate').checked,
+    "Importe": document.getElementById('importeUpdate').value,
+    "Descripcion": document.getElementById('descripcionUpdate').value
+  });
+
+  const options = {
+    method: 'PUT',
+    url: `http://localhost:912/api/pizzas/${document.getElementById('idUpdate').value}`,
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidApi-Key': 'your-rapidapi-key',
+      'X-RapidApi-Host': 'microsoft-translator-text.p.rapidapi.com',
+    },
+    data: params
+  }
+
+  axios
+    .request(options)
+    .then((result) => {
+
       alert("Cambiado con exito");
-      
+
     })
     .catch((error) => {
       console.log('error');
