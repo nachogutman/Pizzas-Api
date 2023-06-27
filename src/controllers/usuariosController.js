da63eebd5aae1c23d3f4a6fc4d959bfc69a7ddef
TOMÁS MARCOS FORNI <47294844@ort>
2023-06-27 08:41:20 -0300

    le damos un token

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -5,9 +5,9 @@ import UsuarioService from '../services/usuarios-services.js';
 const router = Router();
 const svc = new UsuarioService();
 
-router.get('/login', async (req, res) => {
+router.post('/login', async (req, res) => {
     try {
-        let login = await svc.login(req.body.UserName, req.body.PassWord);
+        let login = await svc.login(req.body.UserName, req.body.Password);
         res.send(login);
     } catch (error) {
         res.send("error");
--- src/services/usuarios-services.js
+++ src/services/usuarios-services.js
@@ -1,20 +1,21 @@
 import config from '../../dbconfig.js';
 import sql from 'mssql';
 import Escribir from '../modules/log-helper.js';
+import { randomUUID } from 'crypto';
+
 
 class UsuarioService {
 
     login = async (username, password) => {
         let returnEntity = null;
-        returnEntity = this.getByUserName(username, password);
+        returnEntity = await this.getByUserName(username, password);
         if(returnEntity != null){
-            console.log(returnEntity)
-        }
+            await this.refreshToken(returnEntity.Id);
+        } 
     }
 
     getByUserName = async (username, password) => {
         let returnEntity = null;
-
         try {
             let pool = await sql.connect(config);
             let result = await pool.request()
@@ -24,8 +25,24 @@ class UsuarioService {
             returnEntity = result.recordsets[0][0]
         } catch (error) {
             console.log(error);
-        }
+        } 
         return returnEntity;
+    }
+
+    refreshToken = async (id) => {
+        let rowsAffected = 0;
+        let randomToken = randomUUID();
+        try {
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+                .input('pId', sql.Int, id)
+                .input('pToken', sql.VarChar, randomToken)
+                .query('UPDATE usuarios SET Token=@pToken WHERE Id=@pId');
+            rowsAffected = result.rowsAffected;
+        } catch (error) {
+            console.log(error);
+        }
+        return rowsAffected;
     }
 }
 
